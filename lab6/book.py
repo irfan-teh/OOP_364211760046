65c3c80c0b2d3c73c2f8fe7accb532ea5b4eacf5
irfan_teh <irfan.t@rmutsvmail.com>
2022-05-23 09:07:23 +0700

    test push project

---
+++ lab6/book.py
@@ -0,0 +1,57 @@
+"""
+Student Name: {irfan teh}
+ID: {364211760046}
+Grop: {MIT212}
+"""
+
+"""
+Example:
+class Book
+attributes: book_name(str),price(float),auther(str),publisher(str)
+"""
+
+# create class
+class Book:
+    def __init__(self,bookname,price,auther,publisher):
+        # object attributes
+        self.bookname = bookname
+        self.price = price
+        self.auther = auther
+        self.publisher = publisher
+    def book_detail(self):
+        print(f'Book name: {self.bookname} price: {self.price} THB '
+              f'Auther: {self.auther} Publisher: {self.publisher}')
+
+
+# create objeck
+b1 = Book("OOP",200.00,"Puriwat Lertkrai","MT Familly")
+b2 = Book("Computer Programming",250.00,"Krerkkeat Dummee","RUTS")
+
+# display object attribute
+# print(b1.bookname)
+# print(b1.price)
+# print(b1.auther)
+# print(b1.publisher)
+
+# print(b2.bookname)
+# print(b2.price)
+# print(b2.auther)
+# print(b2.publisher)
+
+# b2.price = 300.00
+# print(b2.price)
+# print(b2.auther)
+
+b1.book_detail()
+b2.book_detail()
+
+# store object into list
+mybook = list([b1,b2])
+print(mybook)
+
+print("Display book from list: ")
+for x in range(len(mybook)):
+    print(mybook[x].book_detail())
+
+for x in mybook:
+    print(x.book_detail())
